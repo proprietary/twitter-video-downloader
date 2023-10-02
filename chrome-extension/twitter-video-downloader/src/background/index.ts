5272739846d61f2ba628301069f4a0a604be706c
Zelly Snyder <zelly@outlook.com>
2023-10-02 13:48:18 -0700

    v1.0.19: update latest X

--- chrome-extension/twitter-video-downloader/src/background/index.ts
+++ chrome-extension/twitter-video-downloader/src/background/index.ts
@@ -87,7 +87,7 @@ async function fetchMainJsContents(mainJsUrl) {
 }
 
 function parseOutAuthToken(mainJsContents) {
-	const re = /"(AAAAAAA[a-zA-Z0-9%]+)"/g;
+	const re = /"Bearer (AAAAAAA[\w%]+)"/g;
 	const r = re.exec(mainJsContents);
 	if (r == null || r.length < 2) {
 		throw new TwitterWebAppBreakingChangeError('failure to find auth token in main.xxxxx.js');
@@ -324,7 +324,7 @@ class TwitterEnvironment {
 
 function tweetDetail(twtrEnv: TwitterEnvironment, tweetId, tweetUsername: string): Promise<VideoItem[]> {
 	let rawVariables: any = {"focalTweetId": tweetId.toString(),"with_rux_injections":false,"includePromotedContent":true,"withCommunity":true,"withQuickPromoteEligibilityTweetFields":true,"withBirdwatchNotes":true,"withVoice":true,"withV2Timeline":true};
-	let rawFeatures: any = {"rweb_lists_timeline_redesign_enabled":true,"responsive_web_graphql_exclude_directive_enabled":true,"verified_phone_label_enabled":false,"creator_subscriptions_tweet_preview_api_enabled":true,"responsive_web_graphql_timeline_navigation_enabled":true,"responsive_web_graphql_skip_user_profile_image_extensions_enabled":false,"tweetypie_unmention_optimization_enabled":true,"vibe_api_enabled":true,"responsive_web_edit_tweet_api_enabled":true,"graphql_is_translatable_rweb_tweet_is_translatable_enabled":true,"view_counts_everywhere_api_enabled":true,"longform_notetweets_consumption_enabled":true,"tweet_awards_web_tipping_enabled":false,"freedom_of_speech_not_reach_fetch_enabled":true,"standardized_nudges_misinfo":true,"tweet_with_visibility_results_prefer_gql_limited_actions_policy_enabled":false,"interactive_text_enabled":true,"responsive_web_text_conversations_enabled":false,"longform_notetweets_rich_text_read_enabled":true,"longform_notetweets_inline_media_enabled":false,"responsive_web_enhance_cards_enabled":false};
+	let rawFeatures: any = {"responsive_web_graphql_exclude_directive_enabled":true,"verified_phone_label_enabled":false,"responsive_web_home_pinned_timelines_enabled":false,"creator_subscriptions_tweet_preview_api_enabled":true,"responsive_web_graphql_timeline_navigation_enabled":true,"responsive_web_graphql_skip_user_profile_image_extensions_enabled":false,"tweetypie_unmention_optimization_enabled":true,"responsive_web_edit_tweet_api_enabled":true,"graphql_is_translatable_rweb_tweet_is_translatable_enabled":true,"view_counts_everywhere_api_enabled":true,"longform_notetweets_consumption_enabled":true,"responsive_web_twitter_article_tweet_consumption_enabled":false,"tweet_awards_web_tipping_enabled":false,"freedom_of_speech_not_reach_fetch_enabled":true,"standardized_nudges_misinfo":true,"tweet_with_visibility_results_prefer_gql_limited_actions_policy_enabled":true,"longform_notetweets_rich_text_read_enabled":true,"longform_notetweets_inline_media_enabled":true,"responsive_web_media_download_video_enabled":false,"responsive_web_enhance_cards_enabled":false};
 	let features: any = encodeURIComponent(JSON.stringify(rawFeatures));
 	let variables: any = encodeURIComponent(JSON.stringify(rawVariables));
 	const graphQlId = twtrEnv.graphQlQueryIds['TweetDetail'];
